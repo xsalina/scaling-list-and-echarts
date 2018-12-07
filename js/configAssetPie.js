328deaae07b4e83f259ee1d218e517e40274e31f
salina <13052519169@126.com>
2018-12-07 13:16:42 +0800

    scale——list

--- js/configAssetPie.js
+++ js/configAssetPie.js
@@ -1,68 +1,22 @@
-function configAssetPie(data) {
-    var pie = document.getElementById("pie1");
-    // 基于准备好的dom，初始化echarts实例
-    var myChart = echarts.init(pie);
-    var dataArr = data;
-    var option = {
-        color: ["#789ce5", "#f99c2d", "#fb6749", "#4fcbff"],
-        legend: {
-            orient: 'vertical',
-            bottom: 56,
-            right: 80,
-            itemGap: 30,
-            itemWidth: 20,
-            itemHeight: 20,
-            icon: 'circle',
-            formatter: function (name) {
-                for (var i = 0; i < dataArr.length; i++) {
-                    if(dataArr[i].name == name) {
-                        return name + ' ('+dataArr[i].value+'元)';
-                    }
-                }
-            },
-            textStyle: {
-                padding: [0, 0, 0, 14],
-                fontSize: 16,
-                color: '#282828'
-            },
-            data: [dataArr[0].name, dataArr[1].name, dataArr[2].name, dataArr[3].name]
-        },
-        tooltip: {
-            trigger: 'item',
-            formatter: "{a} <br/>{b}: {c}元({d}%)"
-        },
-        series: [
-            {
-                name: '已配置的资产总价值',
-                type: 'pie',
-                radius: [55, 85],
-                center: [212, 133],
-                label: {
-                    normal: {
-                        show: true,
-                        textStyle: {
-                            fontSize: 16,
-                            color: "#282828"
-                        }
-                    }
-                },
-                labelLine: {
-                    show: true,
-                    length: 20,
-                    length2: 40
-                },
-                data: dataArr,
-                itemStyle: {
-                    normal:{
-                        label:{
-                            show: true, // {b} data.name {c} data.value {d}% 百分比
-                            formatter: '{d}%'
-                        }
-                    }
-                }
+(function () {
+    var oBtn = document.querySelector('.detail'),
+        aUl = document.querySelectorAll('.bortopright ul'),
+        bool = true;
+        
+        oBtn.onclick = function () {
+        if (bool) {
+            for (let i = 0, length = aUl.length; i < length; i++) {
+                aUl[i].classList.remove("active_height");
+                aUl[i].classList.add("active");
+                oBtn.innerHTML = '返回';
             }
-        ]
-    };
-    // 使用刚指定的配置项和数据显示图表。
-    myChart.setOption(option);
-}
+        } else {
+            for (let j = 0, length = aUl.length; j < length; j++) {
+                aUl[j].classList.remove("active");
+                aUl[j].classList.add("active_height");
+                oBtn.innerHTML = '查看详情';
+            }
+        }
+        bool = !bool;
+    }
+})()
--- js/install.js
+++ js/install.js
@@ -1,21 +1,22 @@
-function inastall() {
+//待安装
+(function () {
     var myChart = echarts.init(document.getElementById('main'));
     option = {
-        color: ['#18B3ee', '#00CBC7', '#BDA0DA'],
+        color: ['#00CBC7', '#18B3ee', '#BDA0DA'],
         tooltip: {
             trigger: 'item',
             formatter: "{a} <br/>{b}: {c} ({d}%)"
         },
-        legend: {
-            orient: 'vertical',
-            x: 'left',
-            data: ['37-72期:60台', '0期:40台', '1-36期:300台']
-        },
+        // legend: {
+        //     orient: 'vertical',
+        //     x: 'left',
+        //      data: [ '0期:40台','1-36期:300台','37-72期:60台']
+        // },
         series: [
             {
                 name: '待安装',
                 type: 'pie',
-                radius: ['50%', '70%'],
+                radius: ['40%', '50%'],
                 avoidLabelOverlap: false,
                 label: {
                     normal: {
@@ -25,7 +26,53 @@ function inastall() {
                     emphasis: {
                         show: true,
                         textStyle: {
-                            fontSize: '30',
+                            fontSize: '17',
+                            fontWeight: 'bold'
+                        }
+                    }
+                },
+                // labelLine: {
+                //     normal: {
+                //         show: true
+                //     }
+                // },
+                data: [
+                    { value: 40, name: '0期' },
+                    { value: 300, name: '1-36期' },
+                    { value: 60, name: '37-72期' },
+                ]
+            }
+        ]
+    };
+    myChart.setOption(option);
+    //收费中
+    var myChart = echarts.init(document.getElementById('charge'));
+    option = {
+        color: ['#18B3ee', '#00CBC7', '#BDA0DA', '#ffb673'],
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
+        // legend: {
+        //     orient: 'vertical',
+        //     x: 'left',
+        //      data: [ '0期', '1-36期','37-72期', '72期以上']
+        // },
+        series: [
+            {
+                name: '收费中',
+                type: 'pie',
+                radius: ['40%', '50%'],
+                avoidLabelOverlap: false,
+                label: {
+                    normal: {
+                        show: true,
+                        //position: 'center'
+                    },
+                    emphasis: {
+                        show: true,
+                        textStyle: {
+                            fontSize: '17',
                             fontWeight: 'bold'
                         }
                     }
@@ -36,16 +83,16 @@ function inastall() {
                     }
                 },
                 data: [
-                    { value: 60, name: '37-72期:60台' },
-                    { value: 40, name: '0期:40台' },
-                    { value: 300, name: '1-36期:300台' }
+                    { value: 40, name: '0期' },
+                    { value: 300, name: '1-36期' },
+                    { value: 60, name: '37-72期' },
+                    { value: 500, name: '72期以上' }
                 ]
             }
         ]
     };
     myChart.setOption(option);
-}
-function confirm() {
+    // 待确认
     var myChart = echarts.init(document.getElementById('confirm'));
     option = {
         color: ['#18B3ee', '#00CBC7', '#BDA0DA'],
@@ -53,16 +100,16 @@ function confirm() {
             trigger: 'item',
             formatter: "{a} <br/>{b}: {c} ({d}%)"
         },
-        legend: {
-            orient: 'vertical',
-            x: 'left',
-            data: ['37-72期:60台', '0期:60台', '1-36期:460台']
-        },
+        // legend: {
+        //     orient: 'vertical',
+        //     x: 'left',
+        //     data: ['37-72期:60台', '0期:40台', '1-36期:300台']
+        // },
         series: [
             {
-                name: '待安装',
+                name: '待确认',
                 type: 'pie',
-                radius: ['50%', '70%'],
+                radius: ['40%', '50%'],
                 avoidLabelOverlap: false,
                 label: {
                     normal: {
@@ -72,7 +119,7 @@ function confirm() {
                     emphasis: {
                         show: true,
                         textStyle: {
-                            fontSize: '30',
+                            fontSize: '17',
                             fontWeight: 'bold'
                         }
                     }
@@ -83,12 +130,64 @@ function confirm() {
                     }
                 },
                 data: [
-                    { value: 60, name: '37-72期:60台' },
-                    { value: 40, name: '0期:60台' },
-                    { value: 300, name: '1-36期:460台' }
+                    { value: 100, name: '0期' },
+                    { value: 300, name: '1-36期' },
+                    { value: 60, name: '37-72期' },
+
                 ]
             }
         ]
     };
     myChart.setOption(option);
-}
+    //机器归属情况
+    var myChart = echarts.init(document.getElementById('machine'));
+    option = {
+        color: ['#00CBC7', '#BDA0DA'],
+        title: {
+            // text: '机器归属情况',
+            left: '20%',
+            top: 10,
+            x: 'center',
+            textStyle: {
+                color: '#999'
+            }
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            y: 'bottom',
+            x: 'left',
+            data: ['平台机器', 'DIV机器']
+        },
+        series: [
+            {
+                name: '机器归属情况',
+                type: 'pie',
+                radius: '55%',
+                center: ['40%', '50%'],
+                roseType: 'radius',
+                data: [
+
+                    { value: 3510, name: '平台机器' },
+                    { value: 435, name: 'DIV机器' },
+                ],
+                labelLine: {
+                    normal: {
+                        show: true
+                    }
+                },
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+    myChart.setOption(option);
+})()
